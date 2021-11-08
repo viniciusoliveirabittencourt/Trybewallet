ae017ca98b9725a36279484effc2190071ad5e6e
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2021-11-07 21:27:51 -0300

    requisiito 7 finalizado

--- src/pages/SelectMoeda.jsx
+++ src/pages/SelectMoeda.jsx
@@ -4,9 +4,31 @@ class SelectMoeda extends React.Component {
   constructor() {
     super();
     this.state = {
-      moeda: 'BRL',
+      moeda: 'USD',
+      arrayMoedas: [],
     };
     this.handleState = this.handleState.bind(this);
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  callAPI() {
+    fetch('https://economia.awesomeapi.com.br/json/all')
+      .then((r) => r.json())
+      .then((r) => {
+        const results = Object.values(r);
+        return this.eliminaDolarTurismo(results);
+      })
+      .then((r) => this.setState({ arrayMoedas: [...r] }))
+      .catch((error) => console.error(error));
+  }
+
+  eliminaDolarTurismo(array) {
+    const findObject = array.find((element) => element.codein === 'BRLT');
+    const arrayIndex = array.indexOf(findObject);
+    return array.splice(arrayIndex);
   }
 
   handleState({ target }) {
@@ -15,12 +37,18 @@ class SelectMoeda extends React.Component {
   }
 
   render() {
-    const { moeda } = this.state;
+    const { moeda, arrayMoedas } = this.state;
     return (
       <label htmlFor="moeda">
         Moeda
         <select id="moeda" onChange={ this.handleState } value={ moeda }>
-          <option value="BRL">BRL</option>
+          { arrayMoedas.map((element, index) => (
+            <option
+              key={ index }
+              value={ element.code }
+            >
+              { element.code }
+            </option>))}
         </select>
       </label>
     );
